bcd0144752188af9dd3f3c6824dc171ed1df03e2
Fabian Hertwig <fabian.hertwig@gmail.com>
2014-12-09 20:35:29 +0100

    moved tkinter mainloop in main thread

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ first_led_offset_num = 17
 leds_in_clockwise_direction = False
 has_corner_leds = False
 
+
 def run_effect():
     """
     Runs the module effect. Copy any hyperion effect code in this module or create your own.
@@ -36,28 +37,26 @@ def run_effect():
 
 
 def main():
-    hyperion.init(horizontal_led_num, vertical_led_num, first_led_offset_num, leds_in_clockwise_direction, has_corner_leds)
+    hyperion.init(horizontal_led_num, vertical_led_num, first_led_offset_num, leds_in_clockwise_direction,
+                  has_corner_leds)
 
-    # create own thread for the gui and the effect as they have to update simultaneously
-    effect_thread = Thread(target=run_effect)
-    window_thread = Thread(target=gui.createWindow)
-
-    effect_thread.start()
-    window_thread.start()
-
-    # Open the connection to the json server after the threads started, so they won't have to wait until
-    # the timeout to start. Uncomment if you do not want to send data to the server.
+    # Open the connection to the json server. Uncomment if you do not want to send data to the server.
     json_client.open_connection(hyperion_host, hyperion_port)
 
-    # wait for the threads to stop
-    window_thread.join()
+    # create own thread for the effect
+    effect_thread = Thread(target=run_effect)
+
+    effect_thread.start()
+    gui.createWindow()
+
     # After the window was closed abort the effect through the fake hyperion module
     hyperion.set_abort(True)
+    # wait for the thread to stop
     effect_thread.join()
 
     # close potential connections
     json_client.close_connection()
-    print "Exiting"
+    print("Exiting")
 
 
 if __name__ == '__main__':
